32a078ac0ccaaa2051c0b7fb12269b1ef7ce2c9c
lulu <lulupiko0347@gmail.com>
2023-06-11 00:04:38 +0800

    test

---
+++ src/main/java/com/example/demo/SpringbootPrac1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPrac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPrac1Application.class, args);
+	}
+
+}
